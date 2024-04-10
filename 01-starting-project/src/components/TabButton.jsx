381c971ba106ad7e5b4b6c65d4e5238771579cba
Clara Kim <02clara.kim@gmail.com>
2024-04-11 01:12:58 +0900

    react pt4 ~66

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,8 +1,8 @@
-export default function TabButton({children, onSelect, isSelected}) {
-//document.querySelector...
-    return (
-        <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
-        </li>
-    );
-}
+export default function TabButton({children, isSelected, ...props}) {
+    //document.querySelector...
+        return (
+            <li>
+                <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
+            </li>
+        );
+    }
