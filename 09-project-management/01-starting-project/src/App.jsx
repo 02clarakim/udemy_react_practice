9e4caf8540b0e7b1d03b91e9bd30f93ff8506cee
Clara Kim <02clara.kim@gmail.com>
2024-05-29 20:47:19 +0900

    5/29

--- 09-project-management/01-starting-project/src/App.jsx
+++ 09-project-management/01-starting-project/src/App.jsx
@@ -1,8 +1,122 @@
+import { useState } from 'react';
+
+import AddNewProject from "./components/AddNewProject";
+import NoProjectYet from "./components/NoProjectYet";
+import Sidebar from "./components/Sidebar";
+import ShowProject from './components/ShowProject';
+
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+    tasks: [],
+  });
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      }
+    });
+  }
+
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
+
+  function handleAddCreate() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null // null means adding project
+      }
+    });
+  }
+
+  function handleCancel() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined // null means adding project
+      }
+    });
+  }
+
+  function handleSave(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  function handleAddTask(text) {
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      }
+
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks]
+      };
+    });
+  }
+
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter(
+          (task) => task.id !== id)
+      };
+    })
+  }
+
+  // find! vanilla js
+  const selectedProject = projectsState.projects.find(project=> project.id === projectsState.selectedProjectId);
+  
+  let content = <ShowProject 
+    project={selectedProject} 
+    onDelete={handleDeleteProject}
+    onAddTask={handleAddTask}
+    onDeleteTask={handleDeleteTask}
+    tasks={projectsState.tasks}
+  />
+
+  if (projectsState.selectedProjectId === null) {
+    content = <AddNewProject onSave={handleSave} onCancel={handleCancel} />
+  } else if (projectsState.selectedProjectId == undefined) {
+    content = <NoProjectYet onAddProject={handleAddCreate}/>
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar 
+        onAddProject={handleAddCreate} 
+        projectList={projectsState.projects}
+        onSelectProject={handleSelectProject}
+        selectedId={projectsState.selectedProjectId}
+      />
+      {content}
+    </main>
   );
 }
 
